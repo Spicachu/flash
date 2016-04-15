e679a8c07e71658205c6189b26d01e5100f379c8
Spicachu <n.a.g.i@live.se>
2016-04-15 19:21:48 +0200

    First Commit

---
+++ src/CFlash.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Anax\Flash;
+ 
+/**
+ * Class for Flashmessages.
+ *
+ */
+class CFlash
+{
+
+    protected $message; // keeps flash message string
+    
+    
+    /**
+     * Contructor for flash message
+     * 
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * flash message (without default class)
+     * @param string $msg message to flash
+     */
+    public function message($msg, $class = null)
+    {
+        // combine msg and class into html
+        $html = "<div class='" . $class . "'>" . $msg . "</div>";
+        
+        // save html 
+        $this->save($html);
+
+        // return html
+        return $html;
+    }
+
+    
+    
+    /**
+     * Error flash message
+     * @param string $msg message to flash
+     * @param string optional replacementclass to append to message
+     * @return string
+     */
+    public function error($msg, $class = "flash-error")
+    {        
+        // pass parameters on to message method
+        $html = $this->message($msg, $class);
+        // return result
+        return $html;
+    }
+
+    /**
+     * Success flash message
+     * @param string $msg message to flash
+     * @param string optional replacementclass to append to message
+     * @return string
+     */
+    public function success($msg, $class = "flash-success")
+    {
+        // pass parameters on to message method
+        $html = $this->message($msg, $class);
+        // return result
+        return $html;
+    }
+
+
+    /**
+     * notice flash message
+     * @param string $msg message to flash
+     */
+    public function notice($msg, $class = "flash-notice")
+    {
+        // pass parameters on to message method
+        $html = $this->message($msg, $class);
+        // return result
+        return $html;
+    }
+
+    /**
+     * output flash message
+     */
+    public function output()
+    {
+        return $this->message;
+    }
+
+    /**
+     * saving to message property
+     * @param string $msg string to save
+     */
+    protected function save($msg)
+    {
+        // concatenate msg with existing messages
+        $this->message .= $msg;
+    }
+}
---
+++ src/CFlashSession.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Anax\Flash;
+
+/**
+ * Class for Flashmessages supporting messages saved in session
+ *
+ */
+class CFlashSession extends CFlash implements \Anax\DI\IInjectionAware
+{
+    use \Anax\DI\TInjectable;
+        
+    /**
+     * constant used as name for sessionvariable  
+     */
+    const SESSION_VARIABLE = 'flashmessage';
+
+    /**
+     * flush message variable from session
+     */
+    public function flush()
+    {
+        $this->session->set(self::SESSION_VARIABLE, null);
+    }
+    
+    /**
+     * output flash message from session
+     */
+    public function output()
+    {
+        if ($this->session->has(self::SESSION_VARIABLE)) {
+            return $this->session->get(self::SESSION_VARIABLE);
+        }
+    }
+
+    /**
+     * saving message to session
+     * @param string $msg message to save
+     */
+    protected function save($msg)
+    {
+        // append not overwrite
+        if ($this->session->has(self::SESSION_VARIABLE)) {
+            $msg = $this->session->get(self::SESSION_VARIABLE) . $msg;
+        }
+        
+        $this->session->set(self::SESSION_VARIABLE, $msg);
+    }
+    
+}
